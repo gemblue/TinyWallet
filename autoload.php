4034eaf59ac692447581aa2ee9be5e2568089fa1
oriza <ahmadoriza@gmail.com>
2020-05-08 00:44:18 +0700

    Add autoload file

---
+++ autoload.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * File autoload
+ * 
+ * @author Gemblue
+ */
+
+include __DIR__ . '/Wallet.php';
+include __DIR__ . '/Repository/Repository.php';
+include __DIR__ . '/Repository/Transaction.php';
+include __DIR__ . '/Repository/Log.php';
+include __DIR__ . '/Repository/Ledger.php';
+include __DIR__ . '/Connection.php';
