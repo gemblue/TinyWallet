f5d46455f922040be567c527db9e944b348c5a9d
oriza <ahmadoriza@gmail.com>
2020-05-08 00:44:44 +0700

    Database config file

---
+++ Config/database.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Config
+ * 
+ * Config array files.
+ * 
+ * @package Config
+ * @author Gemblue
+ */
+
+$config = [
+    'host' => 'localhost',
+    'username' => 'app',
+    'password' => '12345678',
+    'database' => '_wallets'
+];
