aab15e8237c8a75dd64d354e2736f584e80a8cf5
oriza <ahmadoriza@gmail.com>
2020-05-08 00:44:30 +0700

    Simple class for connection

---
+++ Connection.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Connection
+ * 
+ * Simple connection class.
+ * 
+ * @author Gemblue
+ */
+
+namespace Gemblue\Wallet;
+
+class Connection {
+
+    public $host;
+    public $username;
+    public $password;
+    public $database;
+    
+    /**
+     * Construct.
+     */
+    public function __construct() {
+
+        // Inject config.
+        require __DIR__ . '/Config/database.php';
+
+        // Put to property.
+        $this->host = $config['host'];
+        $this->username = $config['username'];
+        $this->password = $config['password'];
+        $this->database = $config['database'];
+    }
+}
+
