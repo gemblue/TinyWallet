17c27bfefda3db71fe0b99b0d44da217b09dfdfe
oriza <ahmadoriza@gmail.com>
2020-05-10 11:55:13 +0700

    Fix autoload.php

--- autoload.php
+++ autoload.php
@@ -6,9 +6,8 @@
  * @author Gemblue.
  */
 
-include __DIR__ . '/src/Repository/Repository.php';
 include __DIR__ . '/src/Repository/Transaction.php';
 include __DIR__ . '/src/Repository/Log.php';
 include __DIR__ . '/src/Repository/Ledger.php';
-include __DIR__ . '/src/Connection.php';
+include __DIR__ . '/src/Factory.php';
 include __DIR__ . '/src/Wallet.php';
